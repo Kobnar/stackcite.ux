b84ce8413b99b621a83691f8700691aebc382d8a
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-06 19:46:39 -0800

    Add basic app scaffold

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// System dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
+
+// StackCite local dependencies
+import { store, history } from './init.js';
+import routes from './app/routes';
+
+// Stylesheets
+import './css/skeleton/normalize.css';
+import './css/skeleton/skeleton.css';
+
+// Call React render method
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      {routes}
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/init.js
@@ -0,0 +1,33 @@
+// System dependencies
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { browserHistory } from 'react-router';
+import { routerReducer, routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+// StackCite locak dependencies
+//import app from './app/reducers';
+
+// Combine the root reducer
+const rootReducer = combineReducers({
+    routing: routerReducer,
+    //app
+})
+
+// Instantiate logger and router middleware
+const loggingMiddleware = createLogger()
+const routingMiddleware = routerMiddleware(browserHistory)
+
+// Instantiate an application-wide state object
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggingMiddleware,
+        routingMiddleware
+    )
+)
+
+// Sync router browser history with application-wide state object
+const history = syncHistoryWithStore(browserHistory, store)
+
+export { store, history };
