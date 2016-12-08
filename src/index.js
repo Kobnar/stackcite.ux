af0ad9b6ac9a966cc057312b3bb9fb2aeb080bbe
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-07 18:34:01 -0800

    Add glyphicons font and css styles from Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-// System dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router'
+import { Provider } from 'react-redux'
 
-// StackCite local dependencies
-import { store, history } from './init.js';
-import routes from './app/routes';
+import { store, history } from './init.js'
+import routes from './app/routes'
 
-// Stylesheets
-import './css/skeleton/normalize.css';
-import './css/skeleton/skeleton.css';
+import './css/skeleton/normalize.css'
+import './css/skeleton/skeleton.css'
+import './css/glyphicons.css'
 
-// Call React render method
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
