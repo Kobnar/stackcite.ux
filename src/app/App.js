f1868bceb0ccdabf027c448c7921b5a4d44df7f7
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-07 21:22:38 -0800

    Add reactive navigation bar

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
+import NavigationBar from './NavigationBar'
+
+import './css/App.css'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        { this.props.children }
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <NavigationBar/>
+                <div className="main-container">
+                    { this.props.children }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
     <div className="container">
-        <h1>Welcom to StackCite!</h1>
+        <h4>Welcome to StackCite!</h4>
 
         <p>Do you read a lot? Do you index what you read? Let me guess, you like to cite the things you read, too.
         Well we have good news: now you have a place to put all those lovely citations.</p>
