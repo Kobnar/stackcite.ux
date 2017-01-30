ada1f3e47f30de695338cfdb5aad3bc411ce4760
Kobnar <konrad.rk.ludwig@gmail.com>
2017-01-30 17:51:43 +0000

    Rearrange ux/actions.js to match other files

--- src/ux/actions.js
+++ src/ux/actions.js
@@ -4,23 +4,24 @@ import { SUCCESS } from '../api/actions'
 import * as actions from '../api/users/auth/actions'
 
 export const HIDE_MOBILE_NAV_MENU = 'HIDE_MOBILE_NAV_MENU'
+export const SHOW_MOBILE_NAV_MENU = 'SHOW_MOBILE_NAV_MENU'
+export const TOGGLE_MOBILE_NAV_MENU = 'TOGGLE_MOBILE_NAV_MENU'
+
 export const hideMobileNavMenu = () => {
     return { type: HIDE_MOBILE_NAV_MENU }
 }
 
-export const SHOW_MOBILE_NAV_MENU = 'SHOW_MOBILE_NAV_MENU'
 export const showMobileNavMenu = () => {
     return { type: SHOW_MOBILE_NAV_MENU }
 }
 
-export const TOGGLE_MOBILE_NAV_MENU = 'TOGGLE_MOBILE_NAV_MENU'
 export const toggleMobileNavMenu = () => {
     return { type: TOGGLE_MOBILE_NAV_MENU }
 }
 
-export const logout = (apiToken) => {
+export const logout = (tokenKey) => {
     return (dispatch) => {
-        return dispatch(actions.deleteAuthToken(apiToken))
+        return dispatch(actions.deleteAuthToken(tokenKey))
             .then(action => {
                 if (action.status === SUCCESS) {
                     return dispatch(push('/login'))
