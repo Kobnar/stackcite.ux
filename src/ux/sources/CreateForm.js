d84522071a3b80cca9e5c41904d3b6fba8dc8dda
Kobnar <konrad.rk.ludwig@gmail.com>
2017-02-06 07:07:58 +0000

    Expand CreateForm with complete BookSource fields

--- src/ux/sources/CreateForm.js
+++ src/ux/sources/CreateForm.js
@@ -6,7 +6,7 @@ import { SUCCESS } from '../../api/actions'
 const SOURCE = 'SOURCE'
 const TEXT = 'TEXT'
 const BOOK = 'BOOK'
-const SOURCE_TYPES = [SOURCE, TEXT, BOOK]
+const SOURCE_TYPES = [BOOK]
 
 const propTypes = {
     onSubmit: React.PropTypes.func.isRequired,
@@ -19,141 +19,235 @@ const defaultProps = {
     errors: {}
 }
 
+const TypeSelection = ({state, onChange}) => {
+    return (
+        <bs.FormGroup>
+            <bs.ControlLabel
+                className="sr-only">
+                Source Type
+            </bs.ControlLabel>
+            <bs.FormControl
+                componentClass="select"
+                value={state}
+                onChange={onChange}>
+                    <option value={BOOK}>Book</option>
+            </bs.FormControl>
+        </bs.FormGroup>
+    )
+}
+
+const TitleInput = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Title
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="title"
+                type="text"
+                placeholder="Title"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const EditionInput = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Edition
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="title"
+                type="text"
+                placeholder="Edition"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const DescriptionTextarea = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Description
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="description"
+                componentClass="textarea"
+                placeholder="Description"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const AuthorsInput = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Authors
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="authors"
+                type="text"
+                placeholder="Authors"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const EditorsInput = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Editors
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="editors"
+                type="text"
+                placeholder="Editors"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const PublisherInput = ({state, error, onChange}) => {
+    return (
+        <bs.FormGroup
+            validationState={ error ? 'error' : null }>
+            <bs.ControlLabel
+                className="sr-only">
+                Publisher
+            </bs.ControlLabel>
+            <bs.FormControl
+                id="publisher"
+                type="text"
+                placeholder="Publisher"
+                value={state}
+                onChange={onChange}/>
+            <bs.HelpBlock>{error}</bs.HelpBlock>
+        </bs.FormGroup>
+    )
+}
+
+const BookControls = ({state, errors, onChange}) => {
+    return (
+        <div>
+            <EditionInput
+                state={state.edition}
+                error={errors.edition}
+                onChange={onChange('edition')} />
+
+            <AuthorsInput
+                state={state.authors}
+                error={errors.authors}
+                onChange={onChange('authors')} />
+
+            <EditorsInput
+                state={state.editors}
+                error={errors.editors}
+                onChange={onChange('editors')} />
+
+            <PublisherInput
+                state={state.publisher}
+                error={errors.publisher}
+                onChange={onChange('publisher')} />
+        </div>
+    )
+}
+
+const emptyForm = {
+    type: BOOK,
+    title: '',
+    description: '',
+    authors: '',
+    editors: '',
+    edition: '',
+    publisher: '',
+    published: '',
+    location: '',
+    isbn13: ''
+}
+
 class CreateForm extends Component {
 
     constructor (props) {
         super(props)
 
-        this.state = {
-            type: TEXT,
-            title: '',
-            description: '',
-            authors: '',
-            editors: '',
-            edition: '',
-            publisher: '',
-            published: '',
-            location: '',
-            isbn13: ''
-        }
+        this.state = { ...emptyForm }
 
-        this.handleTypeChange = this.handleTypeChange.bind(this)
-        this.handleTitleChange = this.handleTitleChange.bind(this)
-        this.handleDescChange = this.handleDescChange.bind(this)
-        this.handleAuthorsChange = this.handleAuthorsChange.bind(this)
-
+        this.onChangeFactory = this.onChangeFactory.bind(this)
         this.handleSubmission = this.handleSubmission.bind(this)
     }
 
-clearForm () {
-    this.setState({
-        title: '',
-        description: ''
-    })
-}
+    clearForm () {
+        this.setState({ ...emptyForm })
+    }
 
-    handleTypeChange (event) { this.setState({ type: event.target.value }) }
-    handleTitleChange (event) { this.setState({ title: event.target.value }) }
-    handleDescChange (event) { this.setState({ description: event.target.value }) }
-    handleAuthorsChange (event) { this.setState({ authors: event.target.value}) }
+    onChangeFactory (field) {
+        return (event) => { this.setState({ [field]: event.target.value })}
+    }
 
     handleSubmission (event) {
         event.preventDefault()
-        this.props.onSubmit({
-            title: this.state.title,
-            description: this.state.description
-        }).then(action => {
-            if (action.status === SUCCESS)
-                this.clearForm()
-        })
-    }
-
-    typeSelection () {
-        return (
-            <bs.FormGroup>
-                <bs.ControlLabel
-                    className="sr-only">
-                    Source Type
-                </bs.ControlLabel>
-                <bs.FormControl
-                    componentClass="select"
-                    value={this.state.type}
-                    onChange={this.handleTypeChange}>
-                        <option value={TEXT}>Text</option>
-                        <option value={BOOK}>- Book</option>
-                </bs.FormControl>
-            </bs.FormGroup>
-        )
-    }
-
-    titleInput (error) {
-        return (
-            <bs.FormGroup
-                validationState={ error ? 'error' : null }>
-                <bs.ControlLabel
-                    className="sr-only">
-                    Title
-                </bs.ControlLabel>
-                <bs.FormControl
-                    id="title"
-                    type="text"
-                    placeholder="Name"
-                    value={this.state.title}
-                    onChange={this.handleTitleChange}/>
-                <bs.HelpBlock>{error}</bs.HelpBlock>
-            </bs.FormGroup>
-        )
-    }
-
-    descInput (error) {
-        return(
-            <bs.FormGroup
-                validationState={ error ? 'error' : null }>
-                <bs.ControlLabel
-                    className="sr-only">
-                    Description
-                </bs.ControlLabel>
-                <bs.FormControl
-                    id="description"
-                    componentClass="textarea"
-                    placeholder="Description"
-                    value={this.state.description}
-                    onChange={this.handleDescChange}/>
-                <bs.HelpBlock>{error}</bs.HelpBlock>
-            </bs.FormGroup>
-        )
-    }
-
-    authorsInput (error) {
-        return(
-            <bs.FormGroup
-                validationState={ error ? 'error' : null }>
-                <bs.ControlLabel
-                    className="sr-only">
-                    Authors
-                </bs.ControlLabel>
-                <bs.FormControl
-                    id="authors"
-                    type="text"
-                    placeholder="Authors"
-                    value={this.state.authors}
-                    onChange={this.handleAuthorsChange}/>
-                <bs.HelpBlock>{error}</bs.HelpBlock>
-            </bs.FormGroup>
-        )
+        // Compile "truthy" values into a form data object
+        var formData = { }
+        Object.entries(this.props.cache.sources)
+            .forEach(([key, value]) => {
+                if (value)
+                    formData[key] = value
+            })
+        // Submit data and clear form
+        this.props.onSubmit(formData)
+            .then(action => {
+                if (action.status === SUCCESS)
+                    this.clearForm()
+            })
     }
 
     render () {
-        var titleError = this.props.errors.title
-        var descError = this.props.errors.description
-        var authorsError = this.props.errors.authors
         return (
             <bs.Form onSubmit={this.handleSubmission}>
 
-                { this.typeSelection() }
-                { this.titleInput(titleError) }
-                { this.state.type === BOOK ? this.authorsInput(authorsError) : null }
-                { this.descInput(descError) }
+                <TypeSelection
+                    state={this.state.type}
+                    onChange={this.onChangeFactory('type')} />
+
+                <TitleInput
+                    state={this.state.title}
+                    error={this.props.errors.title}
+                    onChange={this.onChangeFactory('title')} />
+                
+                { this.state.type === BOOK ?
+                    <BookControls
+                        state={this.state}
+                        errors={this.props.errors}
+                        onChange={this.onChangeFactory} /> : null }
+
+                <DescriptionTextarea
+                    state={this.state.description}
+                    error={this.props.errors.description}
+                    onChange={this.onChangeFactory('description')} />
 
                 <bs.FormGroup
                     className="col-sm-2 pull-right">
