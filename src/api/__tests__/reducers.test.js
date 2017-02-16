6a50e8ce70091a00ce99a4d8f3bff2b4980ff328
Kobnar <konrad.rk.ludwig@gmail.com>
2017-02-16 07:06:55 +0000

    Add tests for cache reducer

--- src/api/__tests__/reducers.test.js
+++ src/api/__tests__/reducers.test.js
@@ -1,7 +1,8 @@
 import deepFreeze from 'deep-freeze'
+import { Schema } from 'normalizr'
 
 import {
-    REST_API,
+    STACKCITE_API,
     POST,
     GET,
     PUT,
@@ -15,23 +16,111 @@ import {
 
 describe('cache', () => {
 
+    const testCollection = 'tests'
+    const testSchema = new Schema(testCollection, { idAttribute: 'id' })
+    const testAction = (method, status, documentId, data, error) => ({
+            type: STACKCITE_API,
+            method,
+            status,
+            data,
+            error,
+            schema: testSchema,
+            collection: testCollection,
+            documentId
+    })
+
     it('does not mutate a given state', () => {
         const existingState = {}
-        const newAction = {
-            type: REST_API,
-            method: POST,
-            status: SUCCESS,
-            data: { items: [] }
-        }
+        const newAction = testAction(
+            POST, SUCCESS, undefined, { id: '58982f1930f193383f952a47' })
         deepFreeze(existingState)
         cache(existingState, newAction)
     })
 
-    it('merges new data if schema set and status SUCCESS', () => {})
+    it('merges new document for a successful POST', () => {
+        const existingState = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' }}}
+        const responseData = { id: '58982f1930f193383f952a4f' }
+        const newAction = testAction(
+            POST, SUCCESS, undefined, responseData)
+        const expected = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' },
+                '58982f1930f193383f952a4f': { id: '58982f1930f193383f952a4f' }
+            }}
+        const result = cache(existingState, newAction)
+        expect(result).toEqual(expected)
+    })
 
-    it('deletes document from cache if method DELETE and status SUCCESS', () => {})
+    it('replaces existing document for a successful document-level GET', () => {
+        const existingState = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': {
+                    id: '58982f1930f193383f952a4b',
+                    name: 'Test Document'},
+                '58982f1930f193383f952a4f': {
+                    id: '58982f1930f193383f952a4f',
+                    name: 'Another Document'}}}
+        const responseData = {
+                id: '58982f1930f193383f952a4b',
+                name: 'Modified Document'
+            }
+        const newAction = testAction(
+            GET, SUCCESS, '58982f1930f193383f952a4b', responseData)
+        const expected = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': {
+                    id: '58982f1930f193383f952a4b',
+                    name: 'Modified Document'},
+                '58982f1930f193383f952a4f': {
+                    id: '58982f1930f193383f952a4f',
+                    name: 'Another Document'}}}
+        const result = cache(existingState, newAction)
+        expect(result).toEqual(expected)
+    })
 
-    it('returns existing state if schema not set and method not DELETE', () => {})
+    it('merges new documents for a successful collection-level GET', () => {
+        const existingState = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' }}}
+        const responseData = {
+            items: [
+                { id: '58982f1930f193383f952a4f' },
+                { id: '5898336130f1933caf7045b4' }
+            ]
+        }
+        const newAction = testAction(POST, SUCCESS, undefined, responseData)
+        const expected = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' },
+                '58982f1930f193383f952a4f': { id: '58982f1930f193383f952a4f' }
+            }}
+    })
 
-    it('returns existing state if status not SUCCESS', () => {})
+    it('deletes existing data for a successful DELETE', () => {
+        const existingState = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' },
+                '58982f1930f193383f952a4f': { id: '58982f1930f193383f952a4f' }
+            }}
+        const newAction = testAction(
+            DELETE, SUCCESS, '58982f1930f193383f952a4f')
+        const expected = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' }}}
+        const result = cache(existingState, newAction)
+        expect(result).toEqual(expected)
+    })
+
+    it('returns existing state if status not SUCCESS', () => {
+        const existingState = {
+            [testCollection]: {
+                '58982f1930f193383f952a4b': { id: '58982f1930f193383f952a4b' },
+                '58982f1930f193383f952a4f': { id: '58982f1930f193383f952a4f' }
+            }}
+        const newAction = testAction(DELETE, FAILURE, '58982f1930f193383f952a4b')
+        const result = cache(existingState, newAction)
+        expect(result).toEqual(existingState)
+    })
 })
