a348c915696bc02b86a8bb98050e343448efc794
Kobnar <konrad.rk.ludwig@gmail.com>
2017-02-16 15:18:59 +0000

    Add mobile NavBar functionality

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux'
 import { store, history } from 'init'
 import routes from 'ux/routes'
 
-import 'css/milligram.css'
 import 'css/glyphicons.css'
+import 'css/milligram.css'
 
 ReactDOM.render(
   <Provider store={store}>
--- src/ux/NavigationBar.js
+++ src/ux/NavigationBar.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import { Link, IndexLink } from 'react-router'
 import { push } from 'react-router-redux'
 
+import { hideNav, toggleNav } from './actions'
 import { logout } from './login/actions'
 
 const NavLink = React.createClass({
@@ -19,6 +21,20 @@ class NavigationBar extends Component {
         this.handleLogout = this.handleLogout.bind(this)
     }
 
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickAway.bind(this), true)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickAway.bind(this), true)
+    }
+
+    handleClickAway (event) {
+        const domNode = ReactDOM.findDOMNode(this)
+        if (!domNode || !domNode.contains(event.target))
+            this.props.dispatch(hideNav())
+    }
+
     handleLogout (event) {
         event.preventDefault()
         var authKey = this.props.auth.token.key
@@ -27,67 +43,82 @@ class NavigationBar extends Component {
     }
 
     render () {
+        var hidden = this.props.hidden
         var isLoggedIn = this.props.auth.token.key
         return (
             <div className='navbar'>
                 <div className='container'>
                     <div className='navbar-header'>
-                        <IndexLink to='/' className='navbar-brand'>Stackcite</IndexLink>
+                        <IndexLink
+                            to='/'
+                            className='navbar-brand'>
+                            Stackcite
+                        </IndexLink>
+                        <div className='navbar-toggle'>
+                            <button
+                                className='button-outline'
+                                onClick={() => this.props.dispatch(toggleNav())}>
+                                <span className='glyphicons glyphicons-menu-hamburger' />
+                            </button>
+                        </div>
                     </div>
-                    <div className='navbar-nav'>
-                        <ul className='navbar-list'>
-                            <li>
-                                <NavLink
-                                    to='/sources'>
-                                    Sources
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to='/people'>
-                                    People
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to='/organizations'>
-                                    Organizations
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='navbar-nav float-right'>
-                        { isLoggedIn ? 
+                    <div
+                        className={'navbar-collapse' + (hidden ? ' hidden' : '')}>
+                        <div className='navbar-nav'>
                             <ul className='navbar-list'>
                                 <li>
                                     <NavLink
-                                        to='/account'>
-                                        Account
+                                        to='/sources'>
+                                        Sources
                                     </NavLink>
                                 </li>
                                 <li>
                                     <NavLink
-                                        to='/logout' 
-                                        onClick={this.handleLogout}>
-                                        Logout
-                                    </NavLink>
-                                </li>
-                            </ul> :
-                            <ul className='navbar-list'>
-                                <li>
-                                    <NavLink
-                                        to='/signup'>
-                                        Signup
+                                        to='/people'>
+                                        People
                                     </NavLink>
                                 </li>
                                 <li>
                                     <NavLink
-                                        to='/login'>
-                                        Login
+                                        to='/organizations'>
+                                        Organizations
                                     </NavLink>
                                 </li>
                             </ul>
-                        }
+                        </div>
+                        <div className='navbar-nav navbar-right'>
+                            { isLoggedIn ? 
+                                <ul className='navbar-list'>
+                                    <li>
+                                        <NavLink
+                                            to='/account'>
+                                            Account
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            to='/logout' 
+                                            onClick={this.handleLogout}>
+                                            Logout
+                                        </NavLink>
+                                    </li>
+                                </ul> :
+                                <ul className='navbar-list'>
+                                    <li>
+                                        <NavLink
+                                            to='/signup'>
+                                            Signup
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            to='/login'>
+                                            Login
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
@@ -96,7 +127,8 @@ class NavigationBar extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.api.auth
+    auth: state.api.auth,
+    hidden: state.ux.navHidden
 })
 
 export default connect(mapStateToProps)(NavigationBar)
